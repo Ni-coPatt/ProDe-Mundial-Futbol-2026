107927ea0579428e6f5da392ac9344efcb40fe3a
Luna Alonso Díaz <lalonsod@fi.uba.ar>
2026-04-18 20:28:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+from flask import Flask
 from flask import request, jsonify
 from flask import Blueprint
 from data.db import get_connection
 
-partidos_bp = Blueprint("partidos", __name__)
+from routes.partidos import partidos_bp
+from routes.usuarios import usuarios_bp
+from routes.predicciones import predicciones_bp
+from routes.ranking import ranking_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(partidos_bp)
+app.register_blueprint(usuarios_bp)
+app.register_blueprint(predicciones_bp)
+app.register_blueprint(ranking_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
